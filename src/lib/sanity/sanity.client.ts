c09a1657895a5c7f89b2e864fefa8fd0d32678a3
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2026-03-11 11:38:22 +0100

    fix-sanity: fix sanity client

--- src/lib/sanity/sanity.client.ts
+++ src/lib/sanity/sanity.client.ts
@@ -4,5 +4,5 @@ export const sanityClient = createClient({
   projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
   dataset: import.meta.env.VITE_SANITY_DATASET,
   apiVersion: import.meta.env.VITE_SANITY_API_VERSION,
-  useCdn: import.meta.env.VITE_SANITY_USE_CDN,
+  useCdn: import.meta.env.VITE_SANITY_USE_CDN === "true",
 });
