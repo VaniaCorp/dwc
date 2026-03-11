e214e11c64a33144b68615d3e040afbe1f9a6f0d
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2026-03-11 18:44:45 +0100

    hot-fix: trail to catch sanity error

--- src/lib/sanity/sanity.client.ts
+++ src/lib/sanity/sanity.client.ts
@@ -5,9 +5,9 @@ let _client: SanityClient | null = null;
 export const getSanityClient = (): SanityClient => {
   if (!_client) {
     _client = createClient({
-      projectId: import.meta.env.VITE_SANITY_PROJECT_ID ?? "",
-      dataset: import.meta.env.VITE_SANITY_DATASET ?? "production",
-      apiVersion: import.meta.env.VITE_SANITY_API_VERSION ?? "2024-06-15",
+      projectId: import.meta.env.VITE_SANITY_PROJECT_ID || "",
+      dataset: import.meta.env.VITE_SANITY_DATASET || "production",
+      apiVersion: import.meta.env.VITE_SANITY_API_VERSION || "2024-06-15",
       useCdn: import.meta.env.VITE_SANITY_USE_CDN === "true",
       stega: false,
     });
